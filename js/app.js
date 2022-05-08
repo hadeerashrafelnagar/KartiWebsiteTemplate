804d3e16260f97b0ca0cf1e54063a01c3cf7a4ea
Hadeer Ashraf El-Nagar <hadeerashrafelnagar@gmail.com>
2022-05-08 16:21:08 +0200

    addwebsitetemplate

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// buttons implemented by Material
+import {MDCRipple} from '@material/ripple';
+
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+// projects section part implemented by Material
+import {MDCTabBar} from '@material/tab-bar';
+
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
